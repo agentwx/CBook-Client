7198fa595e6575c84d7cca0e05171c186f9dbbfe
semdy <501859275@qq.com>
2018-11-22 11:28:55 +0800

    细节优化

---
+++ src/components/badge/index.js
@@ -0,0 +1,19 @@
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  properties: {
+    customClass: {
+      type: String,
+      value: ''
+    },
+    customStyle: {
+      type: String,
+      value: ''
+    },
+    backgroundColor: {
+      type: String,
+      value: '#ff0000'
+    }
+  }
+})
