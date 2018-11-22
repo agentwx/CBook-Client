284b3d34b4841d9008a1544a5025a6fc79dcc533
semdy <501859275@qq.com>
2018-11-22 17:56:02 +0800

    细节优化

--- src/components/weui-loading/index.js
+++ src/components/weui-loading/index.js
@@ -1,4 +1,7 @@
+const computedBehavior = require('miniprogram-computed')
+
 Component({
+  behaviors: [computedBehavior],
   options: {
     addGlobalClass: true
   },
@@ -19,9 +22,20 @@ Component({
       type: Boolean,
       value: true
     },
+    fixedBottom: {
+      type: [Boolean, String],
+      value: false
+    },
     loadingText: {
       type: String,
       value: '正在加载 ...'
+    }
+  },
+  computed: {
+    isFixedBottom () {
+      return typeof this.data.fixedBottom === 'string'
+        ? JSON.parse(this.data.fixedBottom)
+        : this.data.fixedBottom
     }
   }
 })
