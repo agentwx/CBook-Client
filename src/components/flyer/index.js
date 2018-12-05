77a75b42f0a138483ba3cc94cdf5c2ee233940c5
semdy <501859275@qq.com>
2018-12-05 10:56:17 +0800

    细节优化

--- src/components/flyer/index.js
+++ src/components/flyer/index.js
@@ -47,45 +47,91 @@ Component({
     curvature: {
       type: Number,
       value: 0.005
+    },
+    useCss: {
+      type: Boolean,
+      value: true
     }
   },
 
+  data: {
+    animationData: null,
+    innerAnimationData: null
+  },
+
   ready () {
+    const { useCss, duration } = this.data
     this.parabola = null
     this.animation = wx.createAnimation({
-      duration: 0,
+      duration: useCss ? duration : 0,
+      timeingFunction: 'linear'
+    })
+    this.innerAnimation = wx.createAnimation({
+      duration: duration,
       timeingFunction: 'linear'
     })
   },
 
+  detached () {
+    if (this.parabola) {
+      this.parabola.stop()
+    }
+  },
+
   methods: {
     handleShow (value) {
-      if (value === true && !this.parabola) {
-        this.parabola = new Parabola({
-          startX: this.data.startX,
-          startY: this.data.startY,
-          endX: this.data.endX,
-          endY: this.data.endY,
-          curvature: this.data.curvature,
-          duration: this.data.duration,
-          autoStart: this.data.autoStart,
-          complete: (x, y) => {
-            this.parabola = null
-            this.triggerEvent('complete', {value: {x, y}})
-          },
-          step: (x, y) => {
-            this.animation.translate3d(x, y).step()
-            this.setData({
-              animationData: this.animation.export()
-            })
-            this.triggerEvent('step', {value: {x, y}})
-          }
-        })
+      if (value === true) {
+        if (this.data.useCss) {
+          wx.nextTick(() => {
+            this.animateByCss()
+          })
+        } else {
+          this.animateByJs()
+        }
       } else {
         if (this.parabola) {
           this.parabola.stop()
         }
       }
+    },
+    animateByCss () {
+      const { startX, startY, endX, endY, duration } = this.data
+      this.animation.translate3d(0, endY - startY, 0).step()
+      this.innerAnimation.translate3d(endX - startX, 0, 0).step()
+      this.setData({
+        animationData: this.animation.export(),
+        innerAnimationData: this.innerAnimation.export()
+      })
+      setTimeout(() => {
+        this.setData({
+          animationData: null,
+          innerAnimationData: null
+        })
+        this.triggerEvent('complete', {value: {x: endX, y: endY}})
+      }, duration)
+    },
+    animateByJs () {
+      if (this.parabola) return
+      this.parabola = new Parabola({
+        startX: this.data.startX,
+        startY: this.data.startY,
+        endX: this.data.endX,
+        endY: this.data.endY,
+        curvature: this.data.curvature,
+        duration: this.data.duration,
+        autoStart: this.data.autoStart,
+        complete: (x, y) => {
+          this.parabola = null
+          this.triggerEvent('complete', {value: {x, y}})
+        },
+        step: (x, y) => {
+          this.animation.translate3d(x, y).step()
+          this.setData({
+            animationData: this.animation.export()
+          })
+          this.triggerEvent('step', {value: {x, y}})
+        }
+      })
     }
   }
 })
